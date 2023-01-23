04ca08f6f43d396d999638793d1b00c1725f6d9f
jeweljose22 <117150116+jeweljose22@users.noreply.github.com>
2023-01-23 17:51:00 +0530

    Add files via upload

---
+++ pro5/pro5/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("write a dob");
+DateTime dob= Convert.ToDateTime(Console.ReadLine());
+TimeSpan ts1 = DateTime.Now -dob;
+Console.WriteLine("days is " + ts1.Days);
+Console.WriteLine("min:"+ts1.Days*60*60);
+Console.WriteLine("sec:"+ts1.Seconds);
+Console.WriteLine("milsec:"+ts1.Milliseconds);
+Console.WriteLine("year:"+ts1.Days / 365);
+Console.WriteLine("month:"+ts1.Days / 30);
